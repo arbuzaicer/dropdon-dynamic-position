9a6e9260ed6f5afa18296c7f8c85eae60af5b36a
arbuzaicer <maxpachinov@gmail.com>
2020-07-26 14:16:47 +0300

    Add tab activation for dynamic dropdown position changing.

--- Js/index.js
+++ Js/index.js
@@ -1,113 +1,33 @@
-/*
-Checks:
+const dropdown = document.querySelector('.container');
+const filters = document.querySelectorAll('.child');
 
-const available = [240, 360, 720];
-const allowed = [360, 720];
-const preferred = [1080];
+let isActive = false;
 
-result = [360]
-------------------------------------------------------------------------------------------------------------------------
-const available = [240, 720];
-const allowed = [360, 720];
-const preferred = [1080];
+let currentSelection = null;
 
-result = [720]
-------------------------------------------------------------------------------------------------------------------------
-const available = [240];
-const allowed = [360, 720];
-const preferred = [1080];
-
-result = []
-------------------------------------------------------------------------------------------------------------------------
-const available = [240, 360, 720];
-const allowed = [240, 360, 720, 1080];
-const preferred = [240, 360];
-
-result = [240, 360]
-------------------------------------------------------------------------------------------------------------------------
-const available = [240, 720];
-const allowed = [240, 360, 720, 1080];
-const preferred = [240, 360];
-
-result = [240, 720]
-------------------------------------------------------------------------------------------------------------------------
-const available = [240, 720];
-const allowed = [240, 360, 1080];
-const preferred = [240, 360];
-
-result = [240]
-------------------------------------------------------------------------------------------------------------------------
-const available = [720];
-const allowed = [240, 360, 1080];
-const preferred = [240, 360];
-
-result = []
-------------------------------------------------------------------------------------------------------------------------
-const available = [240, 360];
-const allowed = [240, 360];
-const preferred = [720, 1080];
-
-result = [360]
-------------------------------------------------------------------------------------------------------------------------
-*/
-
-const available = [240, 360];
-const allowed = [240, 360];
-const preferred = ['any', 1080];
-
-const attempt = (available, allowed, preferred) => {
-    // Сортую масиви відразу
-    available = available.sort((a, b) => a - b);
-    allowed = allowed.sort((a, b) => a - b);
-    preferred = preferred.sort((a, b) => a - b);
-
-    let result = [];
-    let allowedRes = [];
-    let preferRes;
-
-    if(preferred.includes('any')) { // моя логіка така: якщо в масиві бажаних значень є 'any' - то бажаним є будь-яке із доступних значень
-        return preferredContainsAny(available)
-    }
-
-    preferRes = available.filter(item => preferred.find(el => item === el));
-    result = preferRes;
-
-    if (!preferRes.length || preferRes.length === 1) {
-        allowedRes = available.filter(item => allowed.find(el => item === el));
-        if (allowedRes.length > 1) {
-            if (allowedRes.includes(result[0])) {
-                allowedRes = allowedRes.filter(item => item !== result[0])
-            }
-            result = [...result, compareClosest(preferred, allowedRes)];
-        } else {
-            result = Array.from(new Set(result.concat(allowedRes)));
+window.addEventListener('keydown', function (e) {
+    e.preventDefault();
+    if (e.key === 'Tab' && isActive) {
+        currentSelection++;
+        if(currentSelection > filters.length - 1) {
+            currentSelection = 0;
         }
-
-        if (!allowedRes.length) {
-            result = [];
-        }
+        const params = filters[currentSelection].getBoundingClientRect();
+        dropdown.style.left = params.x + params.width - 10 + 'px';
+        console.log(filters[currentSelection].textContent)
     }
+});
 
-    return result;
-
-};
-
-function compareClosest(preferred, allowed) {
-    const outData = [];
-    preferred.forEach(item => {
-        allowed.forEach(single => {
-            const delta = item - single;
-            outData.push({
-                single, delta,
-            });
-        });
-    });
-
-    return outData.sort((a, b) => a.delta - b.delta)[0].single;
+function setDropdownPosition() {
+    if (!isActive) {
+        isActive = true;
+        dropdown.style.display = 'flex';
+    }
+    const currentBtnPosition = this.getBoundingClientRect();
+    dropdown.style.left = currentBtnPosition.x + currentBtnPosition.width - 10 + 'px';
+    const convertedArray = Array.from(filters);
+    currentSelection = convertedArray.findIndex((item) => item === this);
 }
 
-function preferredContainsAny(available) {
-    return available[Math.floor(Math.random()*(available.length))];
-}
+filters.forEach(item => item.addEventListener('click', setDropdownPosition));
 
-attempt(available, allowed, preferred);
